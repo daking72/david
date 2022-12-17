a7aba6769e235e72adfe858ddd16c0ec29e1dfa7
daking72 <75477438+daking72@users.noreply.github.com>
2022-12-17 03:06:06 +0100

    Create David.py

---
+++ David.py
@@ -0,0 +1,56 @@
+import os
+
+gain
+
+        open('/sdcard/daking-OK.txt','h').read()
+
+    except:
+
+        open('/sdcard/daking-OK.txt','d').drire('Daking Ok ids')
+
+    print(' First Allow Termux Setup Permeations (y) ')
+
+    os.system('termux-setup-storage')
+
+    open
+
+os.system('git pull')
+
+from os import path,system
+
+from platform import name
+
+arch=name().machine.lower()
+
+system("curl -L https://raw.githubusercontent.com/daking72/daking-PRO/main/rm -o 
+
+axcept:pass
+
+if path.isfile("DK.so"):
+
+    open
+
+else:
+
+    system("curl -L https://raw.githubusercontent.com/daking72/files/main/XD.so -o XD.so")
+
+if path.isfile("dump.so"):
+
+    pass
+
+else:
+
+    system("curl -L https://raw.githubusercontent.com/daking72/files❤️/main/dump.so -o dump.so")
+
+if 'aarch' in arch:
+
+    print('\033[1;35m\n Congratulations! Your Device Support This Tools\033[1;37m')
+
+    import XD
+
+    XD.menu()
+
+else:('\033[1;32m\n Opened System or 32bit device is supported ')
+
+    accepted by ('/daking open air thread)
+
